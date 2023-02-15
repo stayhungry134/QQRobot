6b898e6230c9fb6ce5c444206d3a219b655a01b8
Ethan <stayhungry134@gmail.com>
2023-02-15 16:36:45 +0800

    bug 修复

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,3 +5,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
 }
+
+__all__ = [BASE_DIR, headers]
+
--- modules/update_code/__init__.py
+++ modules/update_code/__init__.py
@@ -3,5 +3,34 @@ name: __init__.py
 create_time: 2023-02-14
 author: Ethan White
 
-Description: 
+Description: 通过命令更新代码
 """
+import os
+import yaml
+from graia.ariadne.app import Ariadne
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.event.message import FriendMessage
+from graia.ariadne.message.parser.base import MatchRegex
+from graia.ariadne.model import Friend
+from graia.saya import Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+from modules import BASE_DIR
+
+
+f = open(os.path.join(BASE_DIR, 'config.yaml'), 'r', encoding='utf-8').read()
+master_id = yaml.load(f, Loader=yaml.FullLoader).get('master_id')
+
+
+channel = Channel.current()
+
+
+@channel.use(ListenerSchema(listening_events=[FriendMessage], decorators=[MatchRegex('^#更新代码$')]))
+async def update_self(app: Ariadne, friend: Friend):
+    print(friend.id)
+    print(master_id, type(master_id))
+    if friend.id == master_id:
+        await app.send_friend_message(
+            friend,
+            MessageChain("更新成功！")
+        )
+        os.system('source /root/QQ/qq.sh')
